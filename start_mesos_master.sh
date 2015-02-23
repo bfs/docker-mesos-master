30c35ed5c0785acd6b4711069b3f27f67d3ce1ce
Boris Shimanovsky <boris@factual.com>
2015-02-23 12:48:43 -0800

    making startup a bit more vocal about settings

--- start_mesos_master.sh
+++ start_mesos_master.sh
@@ -2,6 +2,15 @@
 
 #defaults
 export MESOS_WORK_DIR=${MESOS_WORK_DIR:-"/tmp"}
-export MESOS_PORT=${MESOS_PORT:-"5050"}
+export MESOS_PORT=${MESOS_PORT:-${PORT:-5050}}
+export MESOS_HOSTNAME=${MESOS_HOSTNAME:-${HOSTNAME:-`hostname`}}
+
+[ -z "$MESOS_IP" ] && echo "MESOS_IP is not assigned -- this is okay if you're using host networking, but will fail otherwise"
+
+
+echo "Starting Mesos Master"
+echo "-------Mesos ENV Variables-------"
+env | grep MESOS
+echo "----end Mesos ENV Variables----"
 
 /usr/sbin/mesos-master
