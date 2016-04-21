6f0dbd071f104ae46446f31e43227e180a8a5680
Daniel Ochoa <daniel@factual.com>
2016-04-20 20:13:51 -0700

    Update start_mesos_master.sh and README.md - add mesos credentials options

--- start_mesos_master.sh
+++ start_mesos_master.sh
@@ -5,8 +5,13 @@ export MESOS_WORK_DIR=${MESOS_WORK_DIR:-"/tmp"}
 export MESOS_PORT=${MESOS_PORT:-${PORT:-5050}}
 export MESOS_HOSTNAME=${MESOS_HOSTNAME:-${HOSTNAME:-`hostname`}}
 
-[ -z "$MESOS_IP" ] && echo "MESOS_IP is not assigned -- this is okay if you're using host networking, but will fail otherwise"
+if [ ! -z "$MESOS_CREDENTIALS" ]; then
+  echo "$MESOS_CREDENTIALS" > /credentials
+  chmod 600 /credentials
+  export MESOS_CREDENTIALS='/credentials'
+fi
 
+[ -z "$MESOS_IP" ] && echo "MESOS_IP is not assigned -- this is okay if you're using host networking, but will fail otherwise"
 
 echo "Starting Mesos Master"
 echo "-------Mesos ENV Variables-------"
